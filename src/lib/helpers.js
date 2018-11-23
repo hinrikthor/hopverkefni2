4b947c72fb14d631e69f1fd63ee8bbb91462888b
ass106 <43350367+ass106@users.noreply.github.com>
2018-11-23 15:04:09 +0000

    Virknin á forsíðu eiginlega komin

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -11,3 +11,65 @@ export function createElement(el, text) {
   }
   return element;
 }
+
+
+// Function to activate cards and show them.
+export function showCards(value) {
+  let newValue = value.toLowerCase();
+  
+  for (let card of document.querySelectorAll('.card')) {
+    if (card.classList.contains(newValue)) {
+      card.className = `card ${newValue}`;
+    }
+  }
+}
+
+// Function to hide the cards.
+export function hideCards(value) {
+  let newValue = value.toLowerCase();
+  
+  for (let card of document.querySelectorAll('.card')) {
+    if (!card.classList.contains(newValue)) {
+      card.classList.add('card-hidden');
+    } else if (card.classList.contains('card-hidden')) {
+      card.className = `card ${newValue}`;
+    }
+  }
+  
+}
+
+// Variable to count how many buttons are active.
+var buttonCounter = 0;
+
+// Function to read if buttons are active or not.
+// It also runs the functions to hide and show cards 
+// depending on how many buttons are active.
+export function readButton(button) {
+  var bTarget = button.target;
+
+  // If the button is pressed and already active change it to inactive.
+  // raise the counter and if it reaches zero activate all cards.
+  if (bTarget.classList.contains('button-active')) {
+    bTarget.className = 'buttons__button';
+    buttonCounter--;
+    if (buttonCounter === 0) {
+      showCards('html');
+      showCards('css');
+      showCards('javascript');
+    }
+  } else {
+    bTarget.classList.add('button-active');
+    buttonCounter++;
+  }
+
+  // Check the buttons that are active and, for each inner value, hide
+  // specific cards.
+  for (let buttons of document.querySelectorAll('.button-active')) {
+    hideCards(`${buttons.innerHTML}`);
+  }
+
+  for (let buttons of document.querySelectorAll('.button-active')) {
+    showCards(`${buttons.innerHTML}`);
+  }
+  
+}
